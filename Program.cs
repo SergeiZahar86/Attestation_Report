52c28bd2874f5e6846b3820ee283afb10da38922
Сергей Захаров <sergei86zahar@mail.ru>
2020-11-30 17:02:51 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attestation_Report
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            //Console.WriteLine(connectionString);
+            //Console.Read();
+            string sqlExpression = "SELECT * FROM tb_part";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                Console.WriteLine("Подключение открыто");
+                Console.WriteLine("Свойства подключения:");
+                Console.WriteLine("\tСтрока подключения: {0}", connection.ConnectionString);
+                Console.WriteLine("\tБаза данных: {0}", connection.Database);
+                Console.WriteLine("\tСервер: {0}", connection.DataSource);
+                Console.WriteLine("\tВерсия сервера: {0}", connection.ServerVersion);
+                Console.WriteLine("\tСостояние: {0}", connection.State);
+                Console.WriteLine("\tWorkstationld: {0}", connection.WorkstationId);
+                Console.WriteLine("-------------------------");
+
+
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows) // если есть данные
+                {
+                    // выводим названия столбцов
+                    Console.WriteLine("{0}\t{1}\t{2}", reader.GetName(0), reader.GetName(1), reader.GetName(2));
+
+                    while (reader.Read()) // построчно считываем данные
+                    {
+                        object part_id = reader.GetValue(0);
+                        object oper = reader.GetValue(1);
+                        object num_izm = reader.GetValue(2);
+                        object start_time = reader.GetValue(3);
+                        object end_time = reader.GetValue(4);
+                        object num_metering = reader.GetValue(5);
+
+                        Console.WriteLine("{0} \t{1} \t{2} \t{3} \t{4} \t{5}", part_id, oper, num_izm, start_time, end_time, num_metering);
+                    }
+                }
+            }
+            Console.WriteLine("Подключение закрыто...");
+
+            Console.Read();
+        }
+    }
+}
